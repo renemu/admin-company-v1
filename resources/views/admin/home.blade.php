01871101490adbb0bd6e16d7d10ec8c1cb859bf3
Muhammad Rifki Zulfikar <muhamadrifkizulfikar@gmail.com>
2024-11-15 23:38:29 +0700

    edit fix nav and aside bar layouts

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+@extends('layouts.nav')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -7,7 +8,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <!-- <div class="card-body">
+                <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -15,7 +16,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
-                </div> -->
+                </div>
             </div>
         </div>
     </div>
