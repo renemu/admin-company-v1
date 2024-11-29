bd5fe98bb62062b126f4439ac4d393df6501c848
Muhammad Rifki Zulfikar <muhamadrifkizulfikar@gmail.com>
2024-11-29 08:57:42 +0700

    comment in dasahboard for test

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard', function () {
-    return view('admin/index');
-});
+// Route::get('/dashboard', function () {
+//     return view('admin/index');
+// });
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
